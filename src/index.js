2eb5dcadb0d5ac1788c47bd953779b7926765119
CLB <20195357+altyjp@users.noreply.github.com>
2023-03-25 15:26:31 +0900

    Sample usage of redux.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,57 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore} from 'redux'
+
+const initialState = {
+  tasks: []
+}
+
+/**
+ * Reducer, Redux で管理している store(後述) を変化させるための関数
+ * Action を受け取る。
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ */
+const tasksReducer = (state = initialState, action) => {
+  switch(action.type){
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks : state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+
+/**
+ * Action creator, Action を生成する。
+ * Action のテストを行いやすくする効果もある。
+ * @param {*} task 
+ * @returns 
+ */
+const addTask = (task) => (
+  // これがアクションオブジェクト
+  {
+    type: 'ADD_TASK',
+    payload: {
+      task
+    }
+  }
+);
+
+/**
+ * store, アプリケーションで唯一であり、すべての情報を集約する。
+ */
+const store = createStore(tasksReducer);
+
+// store へのアクセスは dispatch に Action を渡す。
+store.dispatch(addTask('猫にごはんをあげる。'));
+
+// getState で中身を確認する。
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
